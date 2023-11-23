ee6204af6078eb34a3d7ed924802bb2c485ad671
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-11-23 20:26:59 +0100

    Add files via upload

---
+++ TP2/TP2 EX1.py
@@ -0,0 +1,11 @@
+print("Avant permutation :")
+x = 1
+y = 2
+print("Valeurs de x:", x)
+print("Valeurs de y:", y)
+print("Après permutation :")
+temp = x
+x = y
+y = temp
+print("Valeurs de x:", x)
+print("Valeurs de y:", y)
