ead2b8376823b9d1a3ce31f89f8c875f8369bdfd
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-11-23 20:35:40 +0100

    Add files via upload

---
+++ TP1/tp1exo3.py
@@ -0,0 +1,6 @@
+print("Saississez un jour, une heure et les minutes dans le code")
+jour = 20
+heure = 21
+minute = 47
+var = ((jour - 1) * 24 * 60) + (1 * heure * 60 + minute)
+print("Nous sommes le {} octobre à {}:{} et il c'est écoulé {} minutes".format(jour, heure, minute, var))
