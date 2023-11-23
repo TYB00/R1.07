0be6a367f9672e747d1a42ca17f28e9522c48a5d
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-11-23 20:22:25 +0100

    Delete ex1.py

--- ex1.py
+++
@@ -1,8 +0,0 @@
-nom = 'toto'
-prenom = 'titi'
-math = 12.5
-anglais = 12.5
-info = 12.5
-promotion = 2023
-var = (math + info + anglais)/3
-print("L'étudiant {} {} de la promotion {} a une moyenne de {}".format(nom, prenom, promotion, var))
