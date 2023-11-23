ace71301ef72d72d9c7b14da45098c471a7a74a2
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-11-23 20:22:07 +0100

    Add files via upload

---
+++ TP1_TB/ex1.py
@@ -0,0 +1,8 @@
+nom = 'toto'
+prenom = 'titi'
+math = 12.5
+anglais = 12.5
+info = 12.5
+promotion = 2023
+var = (math + info + anglais)/3
+print("L'étudiant {} {} de la promotion {} a une moyenne de {}".format(nom, prenom, promotion, var))
---
+++ TP1_TB/ex2.py
@@ -0,0 +1,5 @@
+jour = 17
+heure = 12
+minute = 58
+var = (16 * 24 * 60) + (1 * 12 * 60 + 58)
+print("Nous sommes le {} octobre à {}:{} et il c'est écoulé {}minutes".format(jour, heure, minute, var))
