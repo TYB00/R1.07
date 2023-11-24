33f19e4887c583408dc47b5e02e2b1e148c9c73f
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-11-24 11:13:39 +0100

    Add files via upload

--- TP2/Fondue.py
+++ TP2/Fondue.py
@@ -7,6 +7,6 @@ pain = 400
 nbconvies = int(input("Entrez le nombre de personnes conviées à la fondue : "))
 print("Pour une fondue de ", nbconvies,  "il vous faut :")
 print(f"- {Fromages * nbconvies / Base} gr de fromages")
-print(f"- {eau * nbconvies / Base} litre d'eau")
-print(f"- {ail * nbconvies / Base} gr d'ail")
+print(f"- {eau * nbconvies / Base} décilitre d'eau")
+print(f"- {ail * nbconvies / Base} gousses d'ail")
 print(f"- {pain * nbconvies / Base} gr de pain")
