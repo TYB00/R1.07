5dc9f273865cd52c1c5b7bfd0c1cc97c2cc0b8b3
TYB00 <151144584+TYB00@users.noreply.github.com>
2023-12-08 09:16:44 +0100

    Add files via upload

---
+++ TP3/TP3 EX2 for.py
@@ -0,0 +1,5 @@
+n = int(input("Saisissez un nombre réel positif : "))
+x = n
+for i in range (n+1):
+    print(x)
+    x -= 1
---
+++ TP3/TP3 EX2 while.py
@@ -0,0 +1,5 @@
+n = int(input("Saisissez un nombre réel positif : "))
+x = n
+while x != -1:
+    print(x)
+    x -= 1
